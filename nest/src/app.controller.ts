3bc4c7d4cbd484f5877376a32d23e40e7cdf9e00
zhangchuangm <zhangchuangm@succsoft.cn>
2023-10-15 09:59:09 +0800

    nest-provider

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(
+  //   @Inject(AppService)
+  //   private readonly appService: AppService,
+  //   @Inject('app_service')
+  //   private readonly appService1: AppService,
+  //   @Inject('person')
+  //   private readonly person: any,
+  //   @Inject('person1')
+  //   private readonly person1: any,
+  //   @Inject('person2')
+  //   private readonly person2: any,
+  //   @Inject('person3')
+  //   private readonly person3: any,
+  //   @Inject('person4')
+  //   private readonly person4: any,
+  // ) {}
+  @Inject('app_service')
+  private readonly appService1: AppService;
+  @Inject('person')
+  private readonly person: any;
+  @Inject('person1')
+  private readonly person1: any;
+  @Inject('person2')
+  private readonly person2: any;
+  @Inject('person3')
+  private readonly person3: any;
+  @Inject('person4')
+  private readonly person4: any;
+  @Inject(AppService)
+  private readonly appService: AppService;
 
   @Get()
   getHello(): string {
--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -6,6 +6,54 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'app_service',
+      useClass: AppService,
+    },
+    {
+      provide: 'person',
+      useValue: {
+        name: 'aaa',
+        age: 20,
+      },
+    },
+    {
+      provide: 'person1',
+      useFactory() {
+        return {
+          name: 'aaa',
+          desc: 'ccc',
+        };
+      },
+    },
+    {
+      provide: 'person2',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 100);
+        });
+        return {
+          name: 'bbb',
+          desc: 'ccc',
+        };
+      },
+    },
+    {
+      provide: 'person3',
+      useFactory(person: { name: string }, appService: AppService) {
+        return {
+          name: person.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['person', AppService],
+    },
+    {
+      provide: 'person4',
+      useExisting: 'person2',
+    },
+  ],
 })
 export class AppModule {}
